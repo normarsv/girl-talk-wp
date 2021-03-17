cacb2f69c528689b4109f44423add2c911f8c157
Gerardo <gera91@hotmail.com>
2021-03-17 15:33:11 -0600

    Theme setup Add dev dependencies Update assets loading

--- wp-content/themes/girl-talk/views/layouts/master.blade.php
+++ wp-content/themes/girl-talk/views/layouts/master.blade.php
@@ -5,19 +5,22 @@
     <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
 
-    {{-- <link rel="stylesheet" type="text/css" href="{{ getHashedAsset('css') }}"> --}}
+    <link href="{{ asset('app.css') }}" type="text/css" rel="stylesheet">
 
     {{ wp_head() }}
 
     {{-- @include('partials.favicon') --}}
 </head>
 
-<body {{ body_class() }}>
+<body {{ body_class('bg-accent-light') }}>
 
-{{-- @include('partials.navigation') --}}
+ @include('partials.navigation')
 
 <main class="page-content" id="content">
     @yield('content')
+    <div class="py-52 px-60">
+        <h1 class="font-title text-6xl font-bold text-center">A safe space to give and get advice.</h1>
+    </div>
 </main>
 
 {{-- @include('partials.footer') --}}
