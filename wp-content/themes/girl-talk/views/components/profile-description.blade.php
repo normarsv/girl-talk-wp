bbd7607e41a45831118a0a1649b97d81a75abd8b
Gerardo <gera91@hotmail.com>
2021-04-04 00:32:05 -0600

    About page styles updates

--- wp-content/themes/girl-talk/views/components/profile-description.blade.php
+++ wp-content/themes/girl-talk/views/components/profile-description.blade.php
@@ -2,10 +2,10 @@
     @include('elements.section-title',['text'=>$title])
 
     <div class="flex flex-col md:flex-row items-center justify-start mt-10">
-        <div class="text-xl max-w-650">
+        <div class="text-xl max-w-600">
             {!! $body !!}
         </div>
-        <div class="w-60 mt-14 md:w-full lg:w-60 md:ml-20 lg:ml-40 md:-mt-20">
+        <div class="w-60 mt-14 md:w-full lg:w-64 md:ml-10 lg:ml-20 xl:ml-40 md:mt-0">
             <div class="aspect-w-8 aspect-h-10 rounded shadow-lg shadow-accent">
                 <img src="{{ $image_profile['sizes']['large'] }}" alt="placeholder"
                      class="w-full h-full rounded object-cover">
